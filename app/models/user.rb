a1346425d762b8133da759faaffb86f9b98735ea
cattail96 <cattail264@gmail.com>
2021-04-09 21:51:31 +0900

    ユーザー新規登録・ログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :family_name, presence: true
   validates :first_name, presence: true
-  validates :last_name_kana, presence: true
+  validates :family_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :birthday, presence: true
 
