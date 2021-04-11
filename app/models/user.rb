72506de2c5df51e97539f5fb6f7d042c93392482
cattail96 <cattail264@gmail.com>
2021-04-11 19:31:12 +0900

    Rubocopの実施

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
     validates :nickname
     validates :birthday
-    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
       validates :family_name
       validates :first_name
     end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -73,7 +73,6 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Birthday can't be blank")
       end
 
-
       it 'passwordが5文字以下であれば登録できないこと' do
         @user.password = '12345'
         @user.password_confirmation = '12345'
@@ -114,7 +113,6 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Password can't be blank")
       end
-
     end
   end
 end
