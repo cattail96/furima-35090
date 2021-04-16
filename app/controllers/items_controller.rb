37af8ef2fa57444694da8f20660a302d9d95bf35
cattail96 <cattail264@gmail.com>
2021-04-16 11:34:41 +0900

    Rubocopの実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -47,9 +47,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in? && current_user.id == @item.user.id
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user.id
   end
 
   def find
