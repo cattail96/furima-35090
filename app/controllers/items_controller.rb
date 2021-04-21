686a4c55cb5ec59017d822e614612a5fbe0f6245
cattail96 <cattail264@gmail.com>
2021-04-21 18:25:02 +0900

    編集画面への遷移制限修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -47,7 +47,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user.id || @item.order != nil
+    redirect_to action: :index unless current_user.id == @item.user.id && @item.order == nil
   end
 
   def set_item
